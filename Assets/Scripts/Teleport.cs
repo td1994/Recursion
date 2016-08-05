30da76cdf9ff72e02a0d612e175d03960fb1aa98
Thomas Dowey <Thomas Dowey>
2016-08-04 22:21:22 -0400

    Created new level. Feel free to test it out.

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,9 +5,17 @@ public class Teleport : MonoBehaviour {
 
     public GameObject target;
     public int recursionCount = 1;
-    public int xPos = 0;
+    private float xPos;
     public int toSection;
     public bool beginning;
+
+    void Start()
+    {
+        int i = toSection - 1;
+        PolygonCollider2D startAreaRef = GameObject.Find("EnclosedBarrier" + i).GetComponent<PolygonCollider2D>();
+        GameObject player = GameObject.Find("Character");
+        xPos = startAreaRef.transform.position.x - (startAreaRef.bounds.size.x / 2) + player.GetComponent<BoxCollider2D>().bounds.size.x / 2;
+    }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
